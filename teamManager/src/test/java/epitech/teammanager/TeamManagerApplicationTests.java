190e4b465283460c881d7ef436a25c6b6e864023
ma-gni <157604769+ma-gni@users.noreply.github.com>
2025-10-07 16:52:38 +0200

    the first working file

---
+++ teamManager/src/test/java/epitech/teammanager/TeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package epitech.teammanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
