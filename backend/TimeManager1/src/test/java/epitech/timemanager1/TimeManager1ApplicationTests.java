77e2ed72704df9a8650a4ce884a6ab06d6d5676a
ma-gni <157604769+ma-gni@users.noreply.github.com>
2026-01-11 17:58:20 +0100

    Set active profile to 'test' in application test class
    Added the @ActiveProfiles("test") annotation to ensure tests run with the correct profile configuration. This helps isolate the test environment and prevents unintended interference with other profiles.

--- backend/TimeManager1/src/test/java/epitech/timemanager1/TimeManager1ApplicationTests.java
+++ backend/TimeManager1/src/test/java/epitech/timemanager1/TimeManager1ApplicationTests.java
@@ -2,12 +2,13 @@ package epitech.timemanager1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TimeManager1ApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
